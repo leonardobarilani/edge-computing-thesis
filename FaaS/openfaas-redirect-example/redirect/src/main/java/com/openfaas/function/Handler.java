8ce33dc67551fabf7c71fa7f129751b213e3e32a
leonardobarilani <il_leonardo@hotmail.it>
2022-04-15 20:24:28 +0200

    Updated Redirect example

---
+++ FaaS/openfaas-redirect-example/redirect/src/main/java/com/openfaas/function/Handler.java
@@ -0,0 +1,18 @@
+package com.openfaas.function;
+
+import com.openfaas.model.IHandler;
+import com.openfaas.model.IResponse;
+import com.openfaas.model.IRequest;
+import com.openfaas.model.Response;
+
+public class Handler extends com.openfaas.model.AbstractHandler {
+
+    public IResponse Handle(IRequest req) {
+        Response res = new Response();
+
+        res.setStatusCode(307);
+        res.setHeader("Location", "http://172.27.30.130:31112/function/echo");
+
+	    return res;
+    }
+}
