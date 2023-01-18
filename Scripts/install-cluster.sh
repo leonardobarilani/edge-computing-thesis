5292ee7a1e513ad47af010908b4f2bdae18429e4
fabrizzz <giampietro.fabrizio@mail.polimi.it>
2023-01-18 16:10:52 +0100

    loadbalancer and traefik disabled

--- Scripts/install-cluster.sh
+++ Scripts/install-cluster.sh
@@ -2,24 +2,16 @@
 if [ $# -eq 0 ]
 then
     echo 'One argument needed (will be appended to "k3d-"): profilename'
-    echo 'Optional argument: external port (default: 8081)'
     exit 1
-fi
-
-PORT=8081
-
-if [ $# -eq 2 ]
-then
-    PORT=$2
 fi
 
 kubectl config use-context $1
 
 k3d cluster create $1 \
-    --config $SCRIPTS_PATH/SimpleClusterConfig.yaml
-#    -p "$PORT:80@loadbalancer" \
+    --k3s-arg "--disable=traefik@server:0" \
+    --config $SCRIPTS_PATH/SimpleClusterConfig.yaml  #dove viene settata scrits path???
 
-#it proxies requests to docker hub and redirects them to local-reg
 #k3d kubeconfig merge $1 \
 #    --kubeconfig-merge-default \
 #    --kubeconfig-switch-context
+# export OPENFAAS_URL=http://$(kubectl get nodes -o jsonpath="{.items[0].status.addresses[0].address}"):31112
