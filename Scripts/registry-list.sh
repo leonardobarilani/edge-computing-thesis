cf509e2d043b761cdb6f6a0bd51ec258b3b4b367
fabrizzz <giampietro.fabrizio@mail.polimi.it>
2023-01-18 16:37:24 +0100

    register's name updated

--- Scripts/registry-list.sh
+++ Scripts/registry-list.sh
@@ -1,3 +1,3 @@
 # Run this command to lsit all available containers in the local registry (https://stackoverflow.com/questions/31251356/how-to-get-a-list-of-images-on-docker-registry-v2)
 
-curl -X GET http://myregistry:5000/v2/_catalog
+curl -X GET http://k3d-docker-io.localhost:5000/v2/_catalog
