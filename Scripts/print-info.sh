29d27fbae8d5bcdaf071a286fff7833ff7d4744d
fabrizzz <giampietro.fabrizio@mail.polimi.it>
2023-01-18 16:15:10 +0100

    openfaas password not needed anymore

--- Scripts/print-info.sh
+++ Scripts/print-info.sh
@@ -9,11 +9,7 @@ kubectl config use-context $1
 printf "\n"
 
 echo Entry point:
-echo http://$(kubectl get nodes -o jsonpath="{.items[0].status.addresses[0].address}"):31112
-printf "\n"
-
-echo OpenFaaS password:
-kubectl get secret --namespace openfaas basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode
+echo http://"$(kubectl get nodes -o jsonpath="{.items[0].status.addresses[0].address}")":31112
 printf "\n"
 
 echo Redis password:
