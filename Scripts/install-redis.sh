2be03b776988bd2c111558bd57893705f49bc1a9
fabrizzz <giampietro.fabrizio@mail.polimi.it>
2023-01-19 18:45:09 +0100

    redis password removed

--- Scripts/install-redis.sh
+++ Scripts/install-redis.sh
@@ -9,4 +9,9 @@ kubectl config use-context $1
 printf "\n"
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install my-openfaas-redis bitnami/redis --namespace openfaas-fn --set auth.password=customRedisPassword --set master.persistence.enabled=false --set replica.replicaCount=0
+helm install my-openfaas-redis \
+--namespace openfaas-fn \
+--set auth.enabled=false \
+--set architecture=standalone \
+--set persistence.enabled=false \
+bitnami/redis
