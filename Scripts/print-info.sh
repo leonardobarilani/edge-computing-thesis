d0b1fdcf60fead72534e1bd797e05d71d1973149
fabrizzz <giampietro.fabrizio@mail.polimi.it>
2023-01-19 19:28:21 +0100

    redis password not needed anymore

--- Scripts/print-info.sh
+++ Scripts/print-info.sh
@@ -11,7 +11,3 @@ printf "\n"
 echo Entry point:
 echo http://"$(kubectl get nodes -o jsonpath="{.items[0].status.addresses[0].address}")":31112
 printf "\n"
-
-echo Redis password:
-kubectl get secret --namespace openfaas-fn my-openfaas-redis -o jsonpath="{.data.redis-password}" | base64 --decode
-printf "\n"
