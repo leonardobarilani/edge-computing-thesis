a07144403f0a616dff0ed271dd143d80fca87c47
fabrizzz <giampietro.fabrizio@mail.polimi.it>
2023-01-28 17:42:17 +0100

    bitnami repo moved to first-setup.sh

--- Scripts/install-redis.sh
+++ Scripts/install-redis.sh
@@ -8,7 +8,6 @@ fi
 kubectl config use-context $1
 printf "\n"
 
-helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install my-openfaas-redis \
 --namespace openfaas-fn \
 --set auth.enabled=false \
