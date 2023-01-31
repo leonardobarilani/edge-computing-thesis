fe9e2d2893956934471b418f8246bd6782d94a27
leonardobarilani <il_leonardo@hotmail.it>
2023-01-31 17:21:16 +0100

    Added asserts to tests and automated the tests

--- FaaS/openfaas-offloading-session/tests/test.py
+++ FaaS/openfaas-offloading-session/tests/test.py
@@ -6,14 +6,15 @@
 # p3 test-function
 
 from connection import Connection
+from data_test import DataTest as Data
+import time
 
 session = 'marco'
 con = Connection(node_name='k3d-p3', session=session)
 
-input("Press Enter to begin test.py...")
+assert Data.set_offload_status == con.get('session-offloading-manager?command=set-offload-status&status=accept')
+# input("Press Enter to continue...")
 
-con.get('session-offloading-manager?command=set-offload-status&status=accept')
-input("Press Enter to continue...")
+time.sleep(1)
 
-con.get('session-offloading-manager?command=test-function&session='+session)
-input("Press Enter to continue...")
+assert Data.test_function == con.get('session-offloading-manager?command=test-function&session='+session)
--- FaaS/openfaas-offloading-session/tests/test_onload.py
+++ FaaS/openfaas-offloading-session/tests/test_onload.py
@@ -18,28 +18,30 @@
 
 
 from connection import Connection
+from data_test import DataOnload as Data
+import time
 
 session = 'marco'
 con3 = Connection(node_name='k3d-p3', session=session)
 con2 = Connection(node_name='k3d-p2', session=session)
 
-input("Press Enter to begin test_onload.py...")
+assert Data.test_function_1 == con3.get('session-offloading-manager?command=test-function&session='+session)
+assert Data.test_function_2 == con2.get('session-offloading-manager?command=test-function&session='+session)
+# input("Press Enter to continue...")
+time.sleep(1)
 
-con3.get('session-offloading-manager?command=test-function&session='+session)
-con2.get('session-offloading-manager?command=test-function&session='+session)
-input("Press Enter to continue...")
+assert Data.force_offload == con3.get('session-offloading-manager?command=force-offload', extra_headers={'X-forced-session':'marco'})
+# input("Press Enter to continue...")
+time.sleep(1)
 
-con3.get('session-offloading-manager?command=force-offload', extra_headers={'X-forced-session':'marco'})
-input("Press Enter to continue...")
+assert Data.test_function_3 == con3.get('session-offloading-manager?command=test-function&session='+session)
+assert Data.test_function_4 == con2.get('session-offloading-manager?command=test-function&session='+session)
+# input("Press Enter to continue...")
+time.sleep(1)
 
-con3.get('session-offloading-manager?command=test-function&session='+session)
-con2.get('session-offloading-manager?command=test-function&session='+session)
-input("Press Enter to continue...")
+assert Data.force_onload == con3.get('session-offloading-manager?command=force-onload')
+# input("Press Enter to continue...")
+time.sleep(1)
 
-con3.get('session-offloading-manager?command=force-onload')
-input("Press Enter to continue...")
-
-con3.get('session-offloading-manager?command=test-function&session='+session)
-con2.get('session-offloading-manager?command=test-function&session='+session)
-input("Press Enter to continue...")
-
+assert Data.test_function_5 == con3.get('session-offloading-manager?command=test-function&session='+session)
+assert Data.test_function_6 == con2.get('session-offloading-manager?command=test-function&session='+session)
