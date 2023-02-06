311fa1e58f9106d8a8ea32f108c59ce2599977ae
fabrizzz <giampietro.fabrizio@mail.polimi.it>
2023-02-06 18:30:07 +0100

    now it doesn't change global kubectl context

--- Scripts/watch-clusters.sh
+++ Scripts/watch-clusters.sh
@@ -1,1 +1,1 @@
-watch -n 6 "kubectl config use-context k3d-p3; kubectl get po -A; kubectl config use-context k3d-p2; kubectl get po -A; kubectl config use-context k3d-p1; kubectl get po -A"
+watch "kubectl --context k3d-p1 get po -A; kubectl --context k3d-p2 get po -A; kubectl --context k3d-p3 get po -A"
