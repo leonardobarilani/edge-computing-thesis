5a0bea6345c34dbd9e329553082ca10afa87bf0d
fabrizzz <giampietro.fabrizio@mail.polimi.it>
2023-02-07 19:08:02 +0100

    ip info about node

--- Scripts/offloading-dashboard.sh
+++ Scripts/offloading-dashboard.sh
@@ -1,11 +1,11 @@
-#!/bin/sh 
+#!/bin/sh
 
 tmux new-session -s "thesis" -d
 
 tmux send-keys -t "thesis:0.0" 'htop; exit' Enter
 tmux split-window -v 'watch docker ps'
 tmux split-window -v "watch kubectl get events -A  --sort-by='.metadata.creationTimestamp'"
-tmux split-window -v 'watch kubectl get nodes'
+tmux split-window -v 'watch kubectl get nodes -o wide'
 tmux split-window -h 'watch kubectl get po -A'
 
 tmux resize-pane -t "thesis:0.0" -y 20
