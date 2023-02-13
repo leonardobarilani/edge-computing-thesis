524815a93b236d4fe26594fa0c85a29c09202e99
leonardobarilani <il_leonardo@hotmail.it>
2023-02-13 01:09:43 +0100

    Added missing asserts in shopping cart tests

--- FaaS/shopping-carts/tests/test.py
+++ FaaS/shopping-carts/tests/test.py
@@ -1,6 +1,5 @@
-import time
-
 from connection import Connection
+from data_test import DataTest as Data
 
 con3 = Connection(node_name='k3d-p3')
 con2 = Connection(node_name='k3d-p2')
@@ -8,17 +7,17 @@ con2 = Connection(node_name='k3d-p2')
 session1 = 'marco'
 session2 = 'filippo'
 
-con3.get('shopping-cart?product=kiwi', headers={'X-session':session1})
-con3.get('shopping-cart?product=apple', headers={'X-session':session1})
-con3.get('shopping-cart?product=banana', headers={'X-session':session1})
-con3.get('shopping-cart?product=kiwi', headers={'X-session':session2})
-con3.get('shopping-cart?product=apple', headers={'X-session':session2})
-con3.get('shopping-cart?product=banana', headers={'X-session':session2})
+assert Data.cart1 == con3.get('shopping-cart?product=kiwi', headers={'X-session':session1})
+assert Data.cart2 == con3.get('shopping-cart?product=apple', headers={'X-session':session1})
+assert Data.cart3 == con3.get('shopping-cart?product=banana', headers={'X-session':session1})
+assert Data.cart4 == con3.get('shopping-cart?product=kiwi', headers={'X-session':session2})
+assert Data.cart5 == con3.get('shopping-cart?product=apple', headers={'X-session':session2})
+assert Data.cart6 == con3.get('shopping-cart?product=banana', headers={'X-session':session2})
 
-con2.get('session-offloading-manager?command=test-function&session=counter')
+assert Data.test_function_1 == con2.get('session-offloading-manager?command=test-function&session=counter')
 
-con3.get('shopping-cart?product=kiwi', headers={'X-session':session2})
-con3.get('shopping-cart?product=apple', headers={'X-session':session2})
-con3.get('shopping-cart?product=banana', headers={'X-session':session2})
+assert Data.cart7 == con3.get('shopping-cart?product=kiwi', headers={'X-session':session2})
+assert Data.cart8 == con3.get('shopping-cart?product=apple', headers={'X-session':session2})
+assert Data.cart9 == con3.get('shopping-cart?product=banana', headers={'X-session':session2})
 
-con2.get('session-offloading-manager?command=test-function&session=counter')
+assert Data.test_function_2 == con2.get('session-offloading-manager?command=test-function&session=counter')
