fcc31ecafc0845161a1a8be7a1533050edeccfb2
fabrizzz <giampietro.fabrizio@mail.polimi.it>
2023-02-15 20:34:26 +0100

    improved offloading-dashboard

--- Scripts/offloading-dashboard.sh
+++ Scripts/offloading-dashboard.sh
@@ -3,10 +3,10 @@
 tmux new-session -s "thesis" -d
 
 tmux send-keys -t "thesis:0.0" 'htop; exit' Enter
-tmux split-window -v 'watch docker ps'
-tmux split-window -v "watch kubectl get events -A  --sort-by='.metadata.creationTimestamp'"
-tmux split-window -v 'watch kubectl get nodes -o wide'
-tmux split-window -h 'watch kubectl get po -A'
+tmux split-window -v "kubectl get events -A  --sort-by='.metadata.creationTimestamp' -w"
+tmux split-window -v 'watch kubectl top pods -A'
+tmux split-window -v 'kubectl logs -n openfaas deploy/gateway -f'
+tmux split-window -h 'kubectl get po -A -w'
 
 tmux resize-pane -t "thesis:0.0" -y 20
 tmux resize-pane -t "thesis:0.3" -y 15
