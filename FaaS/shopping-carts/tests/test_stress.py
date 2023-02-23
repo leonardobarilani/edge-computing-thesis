19d2268cb877f31619adcf96ddf5b0d3b3f769c8
leonardobarilani <il_leonardo@hotmail.it>
2023-02-23 17:02:53 +0100

    Implemented Offload Stress test for shopping-carts use case

---
+++ FaaS/shopping-carts/tests/test_stress.py
@@ -0,0 +1,34 @@
+import concurrent
+from concurrent.futures import ThreadPoolExecutor
+from connection import Connection
+
+sessions_count = 5
+products_count = 10
+threads = 10
+
+con3 = Connection(node_name='k3d-p3')
+
+# ------------- 1/3 - Build sessions' requests -------------
+requests = []
+for product in range(0, products_count):
+    for session in range(0, sessions_count):
+        requests.append({"session": "session-" + str(session), "product": "product-" + str(session) + "-" + str(product)})
+
+# ------------- 2/3 - Send requests to populate sessions -------------
+def request(req: dict) -> str:
+    return con3.get('shopping-cart?product=' + req["product"], headers={'X-session': req["session"]})
+
+with ThreadPoolExecutor(max_workers=threads) as executor:
+    future_to_url = {executor.submit(request, req) for req in requests}
+    print("type check: " + str(type(future_to_url)))
+    for future in concurrent.futures.as_completed(future_to_url):
+        try:
+            data = future.result()
+        except Exception as e:
+            print('Exception raised while resolving the future: ', e)
+
+# ------------- 3/3 - Assert correctness of sessions -------------
+for session in range(0, sessions_count):
+    cart = con3.get('session-offloading-manager?command=test-function&type=sessionData&value=session-' + str(session))
+    for product in range(0, products_count):
+        assert 'product-' + str(session) + '-' + str(product) in cart
