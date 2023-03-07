863280dfe7d86458d18e958ef6c97b214a3feec7
leonardobarilani <il_leonardo@hotmail.it>
2023-03-07 01:06:36 +0100

    Updated shopping-carts example (tests and function)

--- FaaS/shopping-carts/tests/test_stress_offloading.py
+++ FaaS/shopping-carts/tests/test_stress_offloading.py
@@ -8,13 +8,14 @@ random.seed(1234)
 sessions_count = 10
 products_count = 20 # per session
 offloads_count = 5 # per session
-threads_count = 10
+threads_count = sessions_count
 delay_between_products_ms = 50 # time to wait if a product fails, before retrying
 delay_between_offloads_ms = 50 # time to wait if an offload fails, before retrying
 
 con3 = Connection(node_name='k3d-p3')
+con2 = Connection(node_name='k3d-p2')
 
-# ------------- 1/3 - Build sessions' requests -------------
+# ------------- 1/5 - Build sessions' requests -------------
 requests = []
 for product in range(0, products_count):
     for session in range(0, sessions_count):
@@ -23,7 +24,7 @@ for session in range(0, sessions_count):
     requests.append({'session': "session-" + str(session)})
 random.shuffle(requests)
 
-# ------------- 2/3 - Send requests to populate sessions -------------
+# ------------- 2/5 - Send requests to populate sessions -------------
 def request(req: dict) -> str:
     if 'product' in req:
         res = 0
@@ -53,10 +54,32 @@ with ThreadPoolExecutor(max_workers=threads_count) as executor:
         except Exception as e:
             print('Exception raised while resolving the future: ', e)
 
-# ------------- 3/3 - Assert correctness of sessions -------------
+# ------------- 3/5 - Retrieve data -------------
+carts = []
 for session in range(0, sessions_count):
-    cart = con3.get('session-offloading-manager?command=test-function&type=sessionData&value=session-' + str(session))[0]
+    cart = con3.get('session-offloading-manager?command=test-function&type=sessionData&value=session-' + str(session))[0] + con2.get('session-offloading-manager?command=test-function&type=sessionData&value=session-' + str(session))[0]
+    carts.append(cart)
+
+# ------------- 4/5 - Assert no data has been lost -------------
+for session in range(0, sessions_count):
     for product in range(0, products_count):
         tested_value = '\\"product-' + str(session) + '-' + str(product) + '\\"'
         print("Tested value: " + tested_value)
-        assert cart.count(tested_value) == 1
+        assert carts[session].count(tested_value) > 0
+print("No data has been lost")
+
+
+# WARNING - this still doesn't test for client centrict consistency
+duplicated_data = False
+# ------------- 5/5 - Check that no data has been duplicated (client centrict consistency successfull) -------------
+for session in range(0, sessions_count):
+    for product in range(0, products_count):
+        tested_value = '\\"product-' + str(session) + '-' + str(product) + '\\"'
+        print("Tested value: " + tested_value)
+        if carts[session].count(tested_value) != 1:
+            duplicated_data = True
+            print("^^^ Duplicated value ^^^")
+if duplicated_data:
+    print("Found some duplicated values")
+else:
+    print("No duplicated values found")
