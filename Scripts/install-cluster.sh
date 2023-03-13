51f2106b19dd8d09ebb48c663aa93d9ccf48d86e
fabrizzz <giampietro.fabriziobonaccorsi@gmail.com>
2023-03-13 17:45:59 +0100

    Configuration to set up external exposed port

--- Scripts/install-cluster.sh
+++ Scripts/install-cluster.sh
@@ -13,5 +13,8 @@ k3d cluster create $1 \
     --k3s-arg "--disable=traefik@server:0" \
     --verbose \
     --trace
+    #uncomment this line to expose the internal port 31112 (openfaas-gateway) to the external 31112 port
+    #if you have multiple cluster on the same computer, remember to expose each cluster on a different PORT (--port "PORT:31112@server:0)
+    # --port "31112:31112@server:0"
 # For extra options, visit:
 # https://k3d.io/v5.4.4/usage/commands/k3d_cluster_create/
