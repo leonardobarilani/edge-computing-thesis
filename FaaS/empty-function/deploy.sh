865ebbc1925f1257efb53b9356ffbc4f2fea6e51
leonardobarilani <il_leonardo@hotmail.it>
2023-03-17 22:54:12 +0100

    Added deploy of empty-function in p1 cluster

--- FaaS/empty-function/deploy.sh
+++ FaaS/empty-function/deploy.sh
@@ -1,5 +1,6 @@
 DEPLOY="java -jar $SCRIPTS_PATH/edge-deployer.jar deploy "
 
+$DEPLOY empty-function $SCRIPTS_PATH/infrastructure.json --inEvery country --inAreas k3d-p1 --faas-cli "--label com.openfaas.scale.min=1"
 $DEPLOY empty-function $SCRIPTS_PATH/infrastructure.json --inEvery city --inAreas k3d-p1 --faas-cli "--label com.openfaas.scale.min=1"
 $DEPLOY empty-function $SCRIPTS_PATH/infrastructure.json --inEvery district --inAreas k3d-p1 --faas-cli "--label com.openfaas.scale.min=1"
 
