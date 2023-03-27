674e30d4800f898ff8e3058f2d20929dcb4feb70
leonardobarilani <il_leonardo@hotmail.it>
2023-03-27 19:24:48 +0200

    Fixed bug in RedisProxyDAO that caused the hanging of JVMs, wasting memory

--- FaaS/openfaas-offloading-session/session-offloading-manager/src/main/java/com/openfaas/function/daos/RedisProxyDAO.java
+++ FaaS/openfaas-offloading-session/session-offloading-manager/src/main/java/com/openfaas/function/daos/RedisProxyDAO.java
@@ -10,22 +10,31 @@ import java.util.Map;
 
 public class RedisProxyDAO {
 
-    private final RedisCommands<String, String> syncCommands;
+    private String url;
+    private RedisClient redisClient;
+    private StatefulRedisConnection<String, String> connection;
     private final HashMap<String, String> localCache;
 
     protected RedisProxyDAO(String table) {
         String host = System.getenv("REDIS_HOST");
         String password = System.getenv("REDIS_PASSWORD");
         String port = System.getenv("REDIS_PORT");
-        String url = "redis://" + password + "@" + host + ":" + port + "/" + table;
+        url = "redis://" + password + "@" + host + ":" + port + "/" + table;
 
         Logger.log("(RedisProxyDAO) (Constructor) Url: " + url);
 
         localCache = new HashMap<>();
+    }
 
-        RedisClient redisClient = RedisClient.create(url);
-        StatefulRedisConnection<String, String> connection = redisClient.connect();
-        syncCommands = connection.sync();
+    private RedisCommands<String, String> openConnection() {
+        redisClient = RedisClient.create(url);
+        connection = redisClient.connect();
+        return connection.sync();
+    }
+
+    private void closeConnection() {
+        connection.close();
+        redisClient.shutdown();
     }
 
     protected Map<String, String> getLocalCache() {
@@ -36,7 +45,9 @@ public class RedisProxyDAO {
         if (localCache.containsKey(key)) {
             return localCache.get(key);
         } else {
+            var syncCommands = openConnection();
             String value = syncCommands.hget(sessionId, key);
+            closeConnection();
             localCache.put(key, value);
             return value;
         }
@@ -49,7 +60,11 @@ public class RedisProxyDAO {
     protected boolean hexists(String sessionId, String key) {
         if (localCache.containsKey(key))
             return true;
-        return syncCommands.hexists(sessionId, key);
+        var syncCommands = openConnection();
+        boolean returnValue = syncCommands.hexists(sessionId, key);
+        closeConnection();
+
+        return returnValue;
     }
 
     protected void hdel(String sessionId, String key) {
