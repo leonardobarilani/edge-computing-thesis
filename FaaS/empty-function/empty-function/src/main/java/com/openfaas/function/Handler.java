af6d7c15a0d7486ec6bc6e6815892a06b3b4a693
fabrizzz <giampietro.fabriziobonaccorsi@gmail.com>
2023-05-09 17:06:01 +0200

    template updated

--- FaaS/empty-function/empty-function/src/main/java/com/openfaas/function/Handler.java
+++ FaaS/empty-function/empty-function/src/main/java/com/openfaas/function/Handler.java
@@ -1,17 +1,13 @@
 package com.openfaas.function;
 
-import com.openfaas.function.api.Offloadable;
-import com.openfaas.model.IHandler;
-import com.openfaas.model.IResponse;
-import com.openfaas.model.IRequest;
-import com.openfaas.model.Response;
+import  com.openfaas.model.*;
 
-public class Handler extends Offloadable {
+public class Handler extends AbstractHandler {
 
-    public IResponse HandleOffload(IRequest req) {
+    public IResponse Handle(IRequest req) {
         Response res = new Response();
-	    res.setBody("Hello, world!");
+        res.setBody("Hello, world!");
 
-	    return res;
+        return res;
     }
 }
