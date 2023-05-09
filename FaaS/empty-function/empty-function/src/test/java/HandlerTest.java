cb0693049121dbaef2073369ff25394b3848a230
fabrizzz <giampietro.fabriziobonaccorsi@gmail.com>
2023-05-09 17:06:01 +0200

    template updated

--- FaaS/empty-function/empty-function/src/test/java/HandlerTest.java
+++ FaaS/empty-function/empty-function/src/test/java/HandlerTest.java
@@ -1,11 +1,12 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 import com.openfaas.function.Handler;
 import com.openfaas.model.IHandler;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
 
 public class HandlerTest {
-    @Test public void handlerIsNotNull() {
+    @Test
+    public void handlerIsNotNull() {
         IHandler handler = new Handler();
         assertNotNull("Expected handler not to be null", handler);
     }
