263e9205baff6ba14c5c25139bfaa66fdb850689
fabrizzz <giampietro.fabriziobonaccorsi@gmail.com>
2023-05-24 17:30:00 +0200

    Example updated with print local ip and hostname instead of "hello world"

--- FaaS/empty-function/empty-function/src/main/java/com/openfaas/function/Handler.java
+++ FaaS/empty-function/empty-function/src/main/java/com/openfaas/function/Handler.java
@@ -2,11 +2,19 @@ package com.openfaas.function;
 
 import  com.openfaas.model.*;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Handler extends AbstractHandler {
 
     public IResponse Handle(IRequest req) {
         Response res = new Response();
-        res.setBody("Hello, world!");
+        try {
+            res.setBody(Inet4Address.getLocalHost().getHostAddress() +"\n"+ InetAddress.getLocalHost().getHostName());
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
 
         return res;
     }
