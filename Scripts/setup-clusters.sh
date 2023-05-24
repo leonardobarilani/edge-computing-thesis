92a91f2c944f153e8be745eabfe43f3e6c79c255
fabrizzz <giampietro.fabriziobonaccorsi@gmail.com>
2023-05-24 17:31:00 +0200

    fixed script with new script names

--- Scripts/setup-clusters.sh
+++ Scripts/setup-clusters.sh
@@ -9,7 +9,7 @@ echo \[WARNING\] By continuing the k3d cluster \"$1\" will be deleted, if it exi
 read
 
 k3d cluster delete $1
-$SCRIPTS_PATH/install-cluster.sh $1
+$SCRIPTS_PATH/install-k3d-cluster.sh $1
 kubectl wait deployment -n kube-system metrics-server local-path-provisioner coredns --for condition=Available=True --timeout=180s
 $SCRIPTS_PATH/install-openfaas.sh k3d-$1
 $SCRIPTS_PATH/install-redis.sh k3d-$1
@@ -17,4 +17,4 @@ $SCRIPTS_PATH/install-redis.sh k3d-$1
 echo
 echo Cluster, openfaas, redis redeployed
 echo
-$SCRIPTS_PATH/print-info.sh k3d-$1
+$SCRIPTS_PATH/print-node-info.sh k3d-$1
