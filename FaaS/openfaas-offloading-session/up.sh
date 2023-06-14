c1b10881af054d3a0f6c04afc59d8c539ab64d04
fabrizzz <giampietro.fabriziobonaccorsi@gmail.com>
2023-06-14 11:27:23 +0200

    comment fixed

--- FaaS/openfaas-offloading-session/up.sh
+++ FaaS/openfaas-offloading-session/up.sh
@@ -4,7 +4,7 @@ faas-cli up --skip-deploy --filter session-offloading-manager || exit 1
 
 echo Deploying
 ./openfaas-offloading-session/deploy.sh || exit 1
-echo "Select 'Python tests in sessions_offloading_manager' in the RUN box"
+echo "Select 'Python tests in sessions_offloading_manager' in the RUN box in the upper-right side corner of intellij"
 echo Run the following commands to analyze the logs:
 echo ../logs.sh k3d-p3
 echo ../logs.sh k3d-p2
