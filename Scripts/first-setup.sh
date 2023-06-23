1b38ad5c37cdfbe63dbbb610d778e03345e597b2
leonardobarilani <il_leonardo@hotmail.it>
2023-06-23 20:36:36 +0200

    Minor changes in first-setup

--- Scripts/first-setup.sh
+++ Scripts/first-setup.sh
@@ -1,27 +1,19 @@
 #Install Docker to be executed without root:
 read -p "Press any key to install docker ..."
-echo 1
 sudo apt-get install ca-certificates curl gnupg lsb-release
-echo 2
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo 25
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-echo 3
 sudo apt update
-echo 4
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 sudo groupadd docker
-echo 5
 sudo usermod -aG docker $USER
-echo 6
 # newgrp docker
-echo 7
 docker run hello-world #check if working
-echo 8
 docker rm -f "$(docker ps -a -q)"
-echo 9
 docker rmi hello-world
+
+echo \[WARNING\] You may want to restart your machine to make sure docker is correctly installed
 
 #Install kubectl:
 read -p "Press any key to install kubectl ..."
@@ -43,6 +35,7 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 #install faas-cli
+read -p "Press any key to install faas-cli ..."
 curl -sSL https://cli.openfaas.com | sudo sh
 
 #install SCRIPTS_PATH and aliases in .bashrc
@@ -61,7 +54,7 @@ fi
 #install autocompletion
 echo \[WARNING\] If you want to install autocompletion \(kubectl, k3d, helm\) scripts in /etc/bash_completion.d folder, press any button, else Ctrl+C to exit the script
 read
-apt-get install bash-completion -y
+sudo apt-get install bash-completion -y
 sudo curl https://raw.githubusercontent.com/docker/cli/master/contrib/completion/bash/docker -o /etc/bash_completion.d/docker
 kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null #optional
 k3d completion bash | sudo tee /etc/bash_completion.d/k3d > /dev/null #optional
