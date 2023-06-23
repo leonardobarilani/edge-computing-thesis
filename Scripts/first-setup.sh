8bda38eff80dcdfe8da086c66b2dca7b1446a50b
leonardobarilani <il_leonardo@hotmail.it>
2023-06-23 20:12:19 +0200

    Minor changes in first-setup

--- Scripts/first-setup.sh
+++ Scripts/first-setup.sh
@@ -1,18 +1,18 @@
 #Install Docker to be executed without root:
 read -p "Press any key to install docker ..."
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+echo 1
+sudo apt-get install ca-certificates curl gnupg lsb-release
+echo 2
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo 25
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo 3
 sudo apt update
+echo 4
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 #sudo groupadd docker
+echo 5
 sudo usermod -aG docker $USER
 newgrp docker
 docker run hello-world #check if working
