d4fff5b319954f8e2cc4ba8fc84bc5f15456052e
leonardobarilani <il_leonardo@hotmail.it>
2023-06-23 20:30:07 +0200

    Minor changes in first-setup

--- Scripts/first-setup.sh
+++ Scripts/first-setup.sh
@@ -27,7 +27,7 @@ docker rmi hello-world
 read -p "Press any key to install kubectl ..."
 sudo apt install -y ca-certificates
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 sudo apt install -y kubectl
 
