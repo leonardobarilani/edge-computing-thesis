8ff075e281274c6880d9d9deaa2314703542fa59
leonardobarilani <il_leonardo@hotmail.it>
2023-06-25 17:26:15 +0200

    Added offload framework to empty-function

--- FaaS/empty-function/empty-function/src/main/java/com/openfaas/function/Handler.java
+++ FaaS/empty-function/empty-function/src/main/java/com/openfaas/function/Handler.java
@@ -1,14 +1,15 @@
 package com.openfaas.function;
 
 import  com.openfaas.model.*;
+import com.openfaas.function.api.Offloadable;
 
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-public class Handler extends AbstractHandler {
+public class Handler extends Offloadable {
 
-    public IResponse Handle(IRequest req) {
+    public IResponse HandleOffload(IRequest req) {
         Response res = new Response();
         try {
             res.setBody(Inet4Address.getLocalHost().getHostAddress() +"\n"+ InetAddress.getLocalHost().getHostName());
