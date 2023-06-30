44b777bf5e382e0c622e582fe22bddb20c59ca00
leonardobarilani <il_leonardo@hotmail.it>
2023-06-30 17:19:38 +0200

    Added script guard in speech-to-text invoke script

--- FaaS/speech-to-text/invoke.sh
+++ FaaS/speech-to-text/invoke.sh
@@ -1,3 +1,9 @@
+# Check if there are exactly 2 arguments
+if [ "$#" -ne 2 ]; then
+    echo "Error: URL required as first argument and filename requred as second argument"
+    exit 1
+fi
+
 curl -X POST \
   -H "X-session: marco" \
   -H "X-session-request-id: $(uuidgen)" \
