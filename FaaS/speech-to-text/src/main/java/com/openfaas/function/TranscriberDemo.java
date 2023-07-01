c496697093b49aba9b89802b36e67e785fa371e0
leonardobarilani <il_leonardo@hotmail.it>
2023-07-01 19:33:17 +0200

    Updated speech-to-text

--- FaaS/speech-to-text/src/main/java/com/openfaas/function/TranscriberDemo.java
+++ FaaS/speech-to-text/src/main/java/com/openfaas/function/TranscriberDemo.java
@@ -1,11 +1,11 @@
 package com.openfaas.function;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import java.util.ArrayList;
 import java.util.Base64;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import edu.cmu.sphinx.api.Configuration;
@@ -14,30 +14,36 @@ import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 
 public class TranscriberDemo {       
 
-    public static String speechToText(String wavInBase64) throws Exception {
+    private static StreamSpeechRecognizer recognizer = null;
 
+    private static void init() throws IOException {
         Configuration configuration = new Configuration();
 
         configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
         configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
         configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
-	StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
+        recognizer = new StreamSpeechRecognizer(configuration);
+    }
+
+    public static String speechToText(String wavInBase64) throws Exception {
+        if (recognizer == null)
+            init();
 	
         byte[] decodedBytes = Base64.getDecoder().decode(wavInBase64);
         InputStream stream = new ByteArrayInputStream(decodedBytes);
 
         recognizer.startRecognition(stream);
-	SpeechResult result;
+        SpeechResult result;
         List<String> stringList = new ArrayList<>();
         while ((result = recognizer.getResult()) != null) {
             String text = result.getHypothesis();
-	    System.out.format("(TranscriberDemo) Hypothesis: %s\n", text);
+	        System.out.format("(TranscriberDemo) Hypothesis: %s\n", text);
             stringList.add(text);
-	}
-	recognizer.stopRecognition();
+	        }
+	    recognizer.stopRecognition();
 	
-	// Build the JSON string manually
+	    // Build the JSON string manually
         StringBuilder jsonBuilder = new StringBuilder();
         jsonBuilder.append("[");
         for (int i = 0; i < stringList.size(); i++) {
@@ -50,7 +56,7 @@ public class TranscriberDemo {
         jsonBuilder.append("]");
         String jsonString = jsonBuilder.toString();
 	
-	return jsonString;
+	    return jsonString;
     }
 }
 
